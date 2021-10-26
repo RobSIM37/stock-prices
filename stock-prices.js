399c41a3355e5005e50d78097c00dec46e8af3df
Robert Lewis <robsim37@gmail.com>
2021-10-26 03:35:05 -0400

    code optimization

--- stock-prices.js
+++ stock-prices.js
@@ -1,18 +1,43 @@
 function best(arr) {
     let bestProfit = 0; // Storage for the result
 
-    for (let i = 0; i < arr.length - 1; i++) { // check all but the last number. It will be compared as a 'Sell' in the last loop
+    // These first two while loop 'pre-treat' the array, removing elements that can't possibly be part of the solution prior to the
+    // search for the solution.
+    let continueLoop = true;
 
-        // In this iteration of the loop, the best selling point is the biggest number in the remaining portion of the array
-        const thisBestSell = arr.slice(i+1,arr[arr.length]).reduce((big, current) => Math.max(big, current));
-        const thisBuy = arr[i]; // the number being considered for the best buy point
-
-        if (thisBestSell - thisBuy > bestProfit) {
-
-            bestProfit = thisBestSell - thisBuy;
-
+    while (arr.length>1 && continueLoop) {
+        if (arr[1] <= arr[0]) {
+            arr.shift();
+        } else {
+            continueLoop = false;
         }
+    }
 
+    continueLoop = true;
+
+    while (arr.length>1 && continueLoop) {
+        if (arr[arr.length - 2] >= arr[arr.length -1]) {
+            arr.pop()
+        } else {
+            continueLoop = false;
+        }
+    }
+
+    if (arr.length > 1) {
+
+        for (let i = 0; i < arr.length - 1; i++) { // check all but the last number. It will be compared as a 'Sell' in the last loop
+
+            // In this iteration of the loop, the best selling point is the biggest number in the remaining portion of the array
+            const thisBestSell = arr.slice(i+1,arr[arr.length]).reduce((big, current) => Math.max(big, current));
+            const thisBuy = arr[i]; // the number being considered for the best buy point
+    
+            if (thisBestSell - thisBuy > bestProfit) {
+    
+                bestProfit = thisBestSell - thisBuy;
+    
+            }
+    
+        }
     }
 
     return bestProfit;
